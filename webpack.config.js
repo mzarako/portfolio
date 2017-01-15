442432166af3222e1fdb7888d176a95cf5399e71
Monika Zarako-Dobrowner <Monika@Monikas-iMac.local>
2017-01-14 19:41:15 -0800

    first commit, basic file setup

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+plugins = [];
+plugins.push(new webpack.optimize.UglifyJsPlugin());
+// plugins.push(new webpack.DefinePlugin({
+//     'process.env': {
+//       'NODE_ENV': JSON.stringify('production')
+//     }
+//   }));
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: plugins,
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './build/'
+  }
+};
